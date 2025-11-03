47bb314e60b69e8a262a649e6dc33c0245d2ff19
Oren Yaar <oren.yaar@humansecurity.com>
2025-11-03 18:06:22 +0200

    v4.3.1

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 let HUMAN_SDK = Target.binaryTarget(
   name: "HUMAN_SDK",
-  url: "https://github.com/PerimeterX/human-security-ios-sdk/releases/download/4.3.0/HUMAN_SDK.xcframework.zip",
-  checksum: "b5750a87281fc17c9fb0461d87d299b7d78b255f92731e12a297b86900be1b19"
+  url: "https://github.com/PerimeterX/human-security-ios-sdk/releases/download/4.3.1/HUMAN_SDK.xcframework.zip",
+  checksum: "8b295100f223092a13ab7cdfc8ae27d49b97925e2bbd2033f790a5c3ed0f058f"
 )
 
 let package = Package(
